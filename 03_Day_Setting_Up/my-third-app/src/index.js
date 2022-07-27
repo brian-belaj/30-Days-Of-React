90b2015591172c34f0c90d1489749a28d256cf1f
Brian Belaj <83066090+brian-belaj@users.noreply.github.com>
2022-07-27 14:56:05 +0200

    Day 2  images

--- 03_Day_Setting_Up/my-third-app/src/index.js
+++ 03_Day_Setting_Up/my-third-app/src/index.js
@@ -1,44 +1,88 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import asabenehImage from './images/asabeneh.jpg'
+
+const user = (
+  <div>
+   <img src={asabenehImage} alt='asabeneh image' />
+  </div>
+)
+const welcome = "My Own 30 days of React";
+const title = "Getting started";
+const subtitle = "Javascript Library";
+const author = {
+  firstName: "Brian Belaj",
+  lastName: "Belaj",
+};
+const date = "Jul 27, 2022";
 
 // JSX element, header
 const header = (
   <header>
     <div className="header-wrapper">
-      <h1>Welcome to 30 Days Of React</h1>
-      <h2>Getting Started React</h2>
-      <h3>JavaScript Library</h3>
-      <p>Asabeneh Yetayeh</p>
-      <small>Oct 2, 2020</small>
+      <h1>{welcome}</h1>
+      <h2>{title}</h2>
+      <h3>{subtitle}</h3>
+      <p>
+        Student: {author.firstName} {author.lastName}
+      </p>
+      <small>Date: {date}</small>
     </div>
   </header>
 );
+const numOne = 3;
+const numTwo = 2;
 
+const result = (
+  <p>
+    {numOne} + {numTwo} = {numOne + numTwo}
+  </p>
+);
 
+const yearBorn = 1988;
+const currentYear = new Date().getFullYear();
+const age = currentYear - yearBorn;
+const personAge = (
+  <p>
+    {" "}
+    {author.firstName} {author.lastName} is {age} years old
+  </p>
+);
+
+const techs = ["HTML", "CSS", "Javascript"];
+const techsFormatted = techs.map((tech) => <li>{tech}</li>);
 // JSX element, main
 const main = (
   <main>
-    <div className="main-wrapper"><p>Prerequisite to get started react.js:</p>
-    <ul>
-      <li>HTML</li>
-      <li>CSS</li>
-      <li>JavaScript</li>
-    </ul></div>
+    <div className="main-wrapper">
+      <p>
+        Prerequisite to get started{" "}
+        <strong>
+          <em>react.js</em>
+        </strong>
+        :
+      </p>
+      <ul>{techsFormatted}</ul>
+      {result}
+      {personAge}
+    </div>
   </main>
-);
+)
+
+const copyright = 'Copyright 2022'
 
 // JSX element, footer
 const footer = (
   <footer>
     <div className="footer-wrapper">
-    <p>Copyright 2020</p>
+      <p>{copyright}</p>
     </div>
   </footer>
 );
 
 // JSX element, app, a container or a parent
 const app = (
-  <div className="app"> 
+  <div className="app">
     {header}
     {main}
     {footer}
@@ -48,4 +92,4 @@ const app = (
 const rootElement = document.getElementById("root");
 // we render the JSX element using the ReactDOM package
 // ReactDOM has the render method and the render method takes two argument
-ReactDOM.render(app, rootElement);
+ReactDOM.render(user, rootElement);
