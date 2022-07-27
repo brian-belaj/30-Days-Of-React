e803e5fa7b803e140148bdc6bf41612070617387
Brian Belaj <83066090+brian-belaj@users.noreply.github.com>
2022-07-27 10:23:57 +0200

    3rd day

---
+++ 03_Day_Setting_Up/my-third-app/src/index.js
@@ -0,0 +1,51 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// JSX element, header
+const header = (
+  <header>
+    <div className="header-wrapper">
+      <h1>Welcome to 30 Days Of React</h1>
+      <h2>Getting Started React</h2>
+      <h3>JavaScript Library</h3>
+      <p>Asabeneh Yetayeh</p>
+      <small>Oct 2, 2020</small>
+    </div>
+  </header>
+);
+
+
+// JSX element, main
+const main = (
+  <main>
+    <div className="main-wrapper"><p>Prerequisite to get started react.js:</p>
+    <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+    </ul></div>
+  </main>
+);
+
+// JSX element, footer
+const footer = (
+  <footer>
+    <div className="footer-wrapper">
+    <p>Copyright 2020</p>
+    </div>
+  </footer>
+);
+
+// JSX element, app, a container or a parent
+const app = (
+  <div className="app"> 
+    {header}
+    {main}
+    {footer}
+  </div>
+);
+
+const rootElement = document.getElementById("root");
+// we render the JSX element using the ReactDOM package
+// ReactDOM has the render method and the render method takes two argument
+ReactDOM.render(app, rootElement);
